66c05408bfc2eb95a1c3469b09cb94f50907f2ce
Bauty Acosta <bauty260106@gmail.com>
2025-03-19 23:31:44 -0300

    segunda consigna

--- actividad1.py
+++ actividad1.py
@@ -22,6 +22,8 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 
+puntaje = 0
+
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -31,6 +33,8 @@ for _ in range(3):
     print(questions[question_index])
     for i, answer in enumerate(answers[question_index]):
         print(f"{i + 1}. {answer}")
+
+    vidas = 2
 
     # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
@@ -49,12 +53,23 @@ for _ in range(3):
         # Se verifica si la respuesta es correcta
         if user_input == correct_answers_index[question_index]:
             print("¡Correcto!")
+            puntaje += 1
             break
         else:
-            # Si el usuario no responde correctamente después de 2 intentos,
-            # se muestra la respuesta correcta
-            print("Incorrecto. La respuesta correcta es:")
-            print(answers[question_index][correct_answers_index[question_index]])
+            print("incorrecto")
+            vidas -= 1
+            puntaje -= 0.5
+
+    if vidas == 0: 
+        # Si el usuario no responde correctamente después de 2 intentos,
+        # se muestra la respuesta correcta
+        print("Incorrecto. La respuesta correcta es:")
+        print(answers[question_index][correct_answers_index[question_index]])
+        
 
     # Se imprime un blanco al final de la pregunta
     print()
+
+print(f"tu puntaje es: {puntaje:.1f} puntos")
+
+# en esta consigna arregle un problema que tenia el programa, que cada vez que te equivocabas, aunque sea el primer error, mostraba la respuesta correcta y despues te dejaba responder
